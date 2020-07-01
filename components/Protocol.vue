16334210c4354fd49f5b746b110b57ece5dc5c70
Kaive Young <kaiveyoung@gmail.com>
2020-07-01 14:04:08 +0800

    init

--- components/Protocol.vue
+++ components/Protocol.vue
@@ -6,7 +6,7 @@
       <el-row>
         <el-col :lg="3" :md="4" :sm="6" :xs="8" v-for="(protocol,index2) in p_list" v-key="index2">
           <div style="height: 100px;width: 150px; display: flex;align-items: center;justify-content: center;">
-            <img style="max-width: 80%; max-height: 60%;" :src="protocol.img" :alt="protocol.name">
+            <img style="max-width: 80%; max-height: 60%;" :src="protocol.img" :alt="protocol.name" :title="protocol.name_zh">
           </div>
         </el-col>
       </el-row>
@@ -26,100 +26,123 @@
       CNC:[
         {
           name:'Fanuc',
-          img: require("~/assets/protocols/fanuc.svg")
+          img: require("~/assets/protocols/fanuc.svg"),
+          name_zh:'发那科'
         },
         {
           name:'Siemens',
-          img: require("~/assets/protocols/siemens.svg")
+          img: require("~/assets/protocols/siemens.svg"),
+          name_zh:'西门子'
         },
         {
           name:'Mitsubishi',
-          img: require("~/assets/protocols/mitsubishi.svg")
+          img: require("~/assets/protocols/mitsubishi.svg"),
+          name_zh:'三菱'
         },
         {
-          name:'Hass',
-          img: require("~/assets/protocols/hass.svg")
+          name:'Haas',
+          img: require("~/assets/protocols/haas.svg"),
+          name_zh:'哈斯'
         },
         {
           name:'Citizen',
-          img: require("~/assets/protocols/citizen.svg")
+          img: require("~/assets/protocols/citizen.svg"),
+          name_zh:'西铁城'
         },
         {
           name:'Brother',
-          img: require("~/assets/protocols/brother.svg")
+          img: require("~/assets/protocols/brother.svg"),
+          name_zh:'发那科'
         },
         {
           name:'Heidenhain',
-          img: require("~/assets/protocols/heidenhain.svg")
+          img: require("~/assets/protocols/heidenhain.svg"),
+          name_zh:'海德汉'
         },
         {
           name:'Mazak',
-          img: require("~/assets/protocols/mazak.svg")
+          img: require("~/assets/protocols/mazak.svg"),
+          name_zh:'马扎克'
         },
         {
           name:'DMG',
-          img: require("~/assets/protocols/dmg.svg")
+          img: require("~/assets/protocols/dmg.svg"),
+          name_zh:'德马吉'
         },
         {
           name:'Mori',
-          img: require("~/assets/protocols/mori.svg")
+          img: require("~/assets/protocols/mori.svg"),
+          name_zh:'森精机'
         },
         {
           name:'Fagor',
-          img: require("~/assets/protocols/fagor.svg")
+          img: require("~/assets/protocols/fagor.svg"),
+          name_zh:'法格'
         },
         {
           name:'KND',
-          img: require("~/assets/protocols/knd.png")
+          img: require("~/assets/protocols/knd.png"),
+          name_zh:'凯恩帝'
         },
         {
           name:'GSK',
-          img: require("~/assets/protocols/gsk.png")
+          img: require("~/assets/protocols/gsk.png"),
+          name_zh:'广数'
         },
         {
           name:'Syntec',
-          img: require("~/assets/protocols/syntec.png")
+          img: require("~/assets/protocols/syntec.png"),
+          name_zh:'新代'
         }
       ],
       PLC:[
         {
           name:'Modbus',
-          img: require("~/assets/protocols/modbus.png")
+          img: require("~/assets/protocols/modbus.png"),
+          name_zh:'Modbus'
         },
         {
           name:'Siemens',
-          img: require("~/assets/protocols/siemens.svg")
+          img: require("~/assets/protocols/siemens.svg"),
+          name_zh:'西门子'
         },
         {
           name:'Mitsubishi',
-          img: require("~/assets/protocols/mitsubishi.svg")
+          img: require("~/assets/protocols/mitsubishi.svg"),
+          name_zh:'三菱'
         },
         {
           name:'Omron',
-          img: require("~/assets/protocols/omron.svg")
+          img: require("~/assets/protocols/omron.svg"),
+          name_zh:'欧姆龙'
         },
         {
           name: 'Schneider',
-          img: require("~/assets/protocols/schneider.svg")
+          img: require("~/assets/protocols/schneider.svg"),
+          name_zh:'施耐德'
         },
         {
-          name:'台达',
-          img: require("~/assets/protocols/delta.png")
+          name:'delta',
+          img: require("~/assets/protocols/delta.png"),
+          name_zh:'台达'
         },
 
       ],
       Robot:[
         {
           name:'Kuka',
-          img: require("~/assets/protocols/kuka.svg")
+          img: require("~/assets/protocols/kuka.svg"),
+          name_zh:'库卡'
         },
         {
           name:'Fanuc',
-          img: require("~/assets/protocols/fanuc.svg")
+          img: require("~/assets/protocols/fanuc.svg"),
+          name_zh:'发那科'
         },
         {
           name:'ABB',
-          img: require("~/assets/protocols/abb.svg")
+          img: require("~/assets/protocols/abb.svg"),
+          name_zh:'ABB'
         },
       ],
     }
