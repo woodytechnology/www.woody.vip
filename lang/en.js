58e6c942296877774aea71fc510447b89db362b0
Kaive Young <kaiveyoung@gmail.com>
2020-09-25 20:01:02 +0800

    修改地址
    Signed-off-by: Kaive Young <kaiveyoung@gmail.com>

---
+++ lang/en.js
@@ -0,0 +1,125 @@
+export default {
+  woody: 'Woody Technology',
+  woody_full: 'Ningxia Woody Technology',
+  title:'Woody Technology|Industrial Data Collection',
+  description: 'Focus on Industrial Internet of Things,Woody Iotbox(MDC) can collect all kinds of CNC/PLC/ROBOT. Include Fanuc, Sismens, Mitsubishi, Haas, Citizen, Brother, Heidenhain, Mazak, DMG, Mori, Fagor, KND, GSK, Syntec, Modbus, Omron, Schneider, Delta, Kuka, ABB etc.',
+  keywords: 'Industrial Internet of Things, IoT, CNC, Data Collection, PLC, MDC, DNC',
+  navbar:{
+    nav:"Nav",
+    more:"More",
+    intro:"Intro",
+    protocol:"Protocols",
+    case:"Case",
+    custom_map:"Customs",
+    contact_us:"Contact Us",
+    posts:"Posts",
+    boxdoc:'Woody IotBox Documents',
+    custom_manager:'Woody IotBox Manager'
+  },
+  app_show:{
+    title:'Woody Industy APP',
+    line1:'Take factory over smart phone',
+    line2:'Support Android and iOS'
+  },
+  box_show:{
+    title:'Woody Iotbox',
+    line1:'Important part of industrial infrastructure',
+    line2:'Support more than 90% CNC, hundreds of PLC',
+    line3:'Remote control. Edge computing'
+  },
+  cloud_show:{
+    title:'Woody Industy Cloud',
+    line1:'Digitalize the factory',
+    line2:'Predictive maintenance. Improve utilization',
+  },
+  intro:{
+    team:{
+      title:'Full-stack Team',
+      line1:'Professional network security',
+      line2:'Experienced machining engineer',
+      line3:'Front-end and back-end teams',
+      line4:'Embedded Geek',
+    },
+    service:{
+      title:'Quick Response',
+      line1:'Response within 2 hours',
+      line2:'365 days customer service',
+      line3:'Fix bugs within 24 hours',
+      line4:'Fast function iteration',
+    },
+    development:{
+      title:'Customized Hardware And Software',
+      line1:'Improve old machine tools',
+      line2:'Dedicated sensor design',
+      line3:'Business platform development',
+      line4:'Connect with private cloud platform',
+    },
+    collection:{
+      title:'Professional Data Collection',
+      line1:'Support hundreds of devices',
+      line2:'Support dozens of platforms',
+      line3:'Complete edge computing',
+      line4:'Cheap. Easy. Low energy consumption',
+    }
+  },
+  protocol:{
+    title:'Support Protocols',
+    fanuc:'Fanuc',
+    siemens:'Siemens',
+    mitsubishi:'Mitsubishi',
+    haas:'Haas',
+    citizen:'Citizen',
+    brother:'Brother',
+    heidenhain:'Heidenhain',
+    mazak:'Mazak',
+    dmg:'DMG',
+    mori:'Mori',
+    fagor:'Fagor',
+    knd:'KND',
+    gsk:'GSK',
+    syntec:'Syntec',
+    modbus:'Modbus',
+    opcua:'OPC Unified Architecture',
+    omron:'Omron',
+    schneider:'Schneider',
+    delta:'Delta',
+    ab:'Allen Bradley',
+    kuka:'Kuka',
+    abb:'ABB'
+  },
+  case:{
+    digital_factory:{
+      title:'Digital Factory',
+      subtitle:'Renovate plants, connect old equipment',
+      content:'Connect to the existing interface of the machine tool to obtain data. Combine MES and ERP to manage factory production scheduling and track production progress. Cooperate with inspection line to calculate equipment utilization and OEE. According to data monitoring such as tool usage, equipment load, etc., the machine tool can be maintained predictably to improve production safety.',
+    },
+    vpn:{
+      title:'Remote Maintenance',
+      subtitle:'Secure virtual private network',
+      content:'Help companies manage remote equipment. Program debugging and updating and equipment maintenance work through VPN network. Customized VPN server to realize user isolation and prevent unauthorized access to improve device security',
+    },
+    iot:{
+      title:'Connect Everything',
+      subtitle:'Create an IoT society',
+      content:'The existing infrastructure is connected to the Internet through the Internet of Things gateway, including various electric meters, field stations, household appliances, cold storage air conditioners, agricultural equipment, etc. Provide stable and reliable data for platform applications such as smart cities and smart agriculture',
+    }
+  },
+  custom:{
+    title:'Custom Map',
+    hanyun:'Hanyun',
+    casi:'CASI Cloud',
+    odot:'odot',
+    zju:'Research Institute of Zhejiang University-Taizhou',
+    xuanji:'Xuanji Industry',
+    shhsmart:'Shuanghong Smart'
+  },
+  footer:{
+    contact:{
+      title:'Contact Us',
+      address:'Address',
+      woody_address:'Room 1305, Building 1, No. 490, S.St. Ning\'an, Jinfeng, Yinchuan, Ningxia',
+      email:'Email',
+      phone:'Phone'
+    }
+  }
+}
